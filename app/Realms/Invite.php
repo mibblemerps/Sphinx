df7259ebe2f022549ee0b4c0daac58246c597372
Mitchfizz05 <me@mitchfizz05.net>
2016-02-23 18:08:09 +1030

    Add server relationship and to mutator/accessor.

--- app/Realms/Invite.php
+++ app/Realms/Invite.php
@@ -18,4 +18,42 @@ class Invite extends Model
         return 'U';
     }
 
+    /**
+     * Server relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function server()
+    {
+        return $this->belongsTo('App\Realms\Server');
+    }
+
+    /**
+     * Mutator for to attribute, encodes player objects into json.
+     *
+     * @param $value
+     * @return string
+     */
+    public function setToAttribute($value)
+    {
+        $this->attributes['to'] = json_encode([
+            'uuid' => $value->uuid,
+            'username' => $value->username
+        ]);
+    }
+
+    /**
+     * Accessor for to attribute, decodes player objects from json.
+     *
+     * @param $value
+     * @return Player
+     */
+    public function getToAttribute($value)
+    {
+        $playerJson = json_decode($value);
+        return new Player(
+            $playerJson->uuid,
+            $playerJson->username
+        );
+    }
 }
