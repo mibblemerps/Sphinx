180f9191a2cb5cd6719853a7c5e2081e16a06971
Mitchfizz05 <me@mitchfizz05.net>
2016-02-23 17:04:25 +1030

    Add migration for invites table.

---
+++ database/migrations/2016_02_23_062435_create_invites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('realm_id');
+            $table->string('to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invites');
+    }
+}
