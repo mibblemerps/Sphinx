69ca06697487e58e23edd9b790a1ba2e1d122756
Mitchfizz05 <me@mitchfizz05.net>
2016-02-26 21:43:31 +1030

    Moved Sphinx Server into the correct directory.

---
+++ Sphinx-Server/app/Http/Controllers/NodeApi/PingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\NodeApi;
+
+use App\Http\Controllers\Controller;
+
+class PingController extends Controller
+{
+    public function ping()
+    {
+
+    }
+}
---
+++ Sphinx-Server/resources/views/sphinx/status.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sphinx Node Status</title>
+    </head>
+    <body>
+
+    </body>
+</html>
