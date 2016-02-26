b7c10a98ec2f62fe467b6eed04071bcc5bd45fd1
Mitchfizz05 <me@mitchfizz05.net>
2016-02-27 02:59:06 +1030

    Request manifest API endpoint now verifies IPs.

--- Sphinx-Server/app/Http/Controllers/NodeApi/ManifestController.php
+++ Sphinx-Server/app/Http/Controllers/NodeApi/ManifestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\NodeApi;
 use App\Facades\SphinxNode;
+use Illuminate\Http\Request;
 
 /**
  * Sphinx API controller for requesting the manifest.
@@ -13,10 +14,17 @@ class ManifestController
 {
     /**
      * Get the entire server manifest.
+     *
+     * @param Request $request
      */
-    public function request()
+    public function request(Request $request)
     {
+        if ($request->ip() != explode(':', env('SPHINX_NODE_ADDRESS'))[0]) {
+            // IP address mismatch.
+            abort(403); // 403 Forbidden.
+        }
+
+        // Send manifest.
         SphinxNode::sendManifest();
-        return 'sent';
     }
 }
