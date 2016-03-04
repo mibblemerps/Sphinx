289ec70dac0574f2c567057b2d911d7edb38346f
Mitchfizz05 <me@mitchfizz05.net>
2016-03-04 17:48:06 +1030

    Add warning if Sphinx Node is offline.

--- Sphinx-Server/resources/views/home.blade.php
+++ Sphinx-Server/resources/views/home.blade.php
@@ -2,5 +2,11 @@
 
 @section('content')
     <h1>Sphinx Dashboard</h1>
-    
+
+    @if(!\App\Facades\SphinxNode::ping())
+        <div class="alert alert-danger">
+            <strong>Important!</strong> The Sphinx Node is currently unreachable!<br>
+            Realms cannot be updated and are likely offline.
+        </div>
+    @endif
 @endsection
