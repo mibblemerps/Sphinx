319232a05a5b02416ab7ab7a0c6f0fe90529b280
Mitchfizz05 <me@mitchfizz05.net>
2016-03-05 01:52:29 +1030

    Eloquent based authentication configured.

--- Sphinx-Server/app/Providers/AuthServiceProvider.php
+++ Sphinx-Server/app/Providers/AuthServiceProvider.php
@@ -26,5 +26,17 @@ class AuthServiceProvider extends ServiceProvider
     {
         // Register Minecraft authenticator.
         $this->app['minecraft_auth'] = new MinecraftAuth($this->app['request']);
+
+        // Register dashboard authentication services.
+        config('auth.defaults.guard', 'web');
+        config('auth.guards', [
+            'web' => ['driver' => 'session', 'provider' => 'users']
+        ]);
+        config('auth.providers', [
+            'users' => [
+                'driver' => 'eloquent',
+                'model' => \App\User::class
+            ]
+        ]);
     }
 }
