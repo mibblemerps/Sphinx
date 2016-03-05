30f769008751c1ed1db6c2bc36bc27c9675c6e2f
Mitchfizz05 <me@mitchfizz05.net>
2016-03-05 20:30:48 +1030

    Add stats to dashboard.

--- Sphinx-Server/app/Http/Controllers/Dashboard/DashboardController.php
+++ Sphinx-Server/app/Http/Controllers/Dashboard/DashboardController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Facades\SphinxNode;
+use App\Realms\Server;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+        // Get stats.
+        $nodeOnline = SphinxNode::ping();
+
+        // Get stats from Node.
+        if ($nodeOnline) {
+            $stats = SphinxNode::stats();
+            $stats['nodeOnline'] = true;
+        } else {
+            $stats = [
+                'nodeOnline' => $nodeOnline,
+                'serversRunning' => 'N/A',
+                'onlinePlayers' => 'N/A'
+            ];
+        }
+
+        // Get other stats.
+        $stats['realmCount'] = Server::count();
+
+        return view('dashboard', ['stats' => $stats]);
     }
 }
--- Sphinx-Server/resources/views/dashboard.blade.php
+++ Sphinx-Server/resources/views/dashboard.blade.php
@@ -1,12 +1,40 @@
 @extends('common.template')
 
-@section('content')
-    <h1>Hello, {{ Auth::user()->username }}!</h1>
+@section('head')
+    <link rel="stylesheet" href="{{ url('/css/dashboard.css') }}">
+@endsection
 
-    @if(!\App\Facades\SphinxNode::ping())
+@section('content')
+    @if(!$stats['nodeOnline'])
         <div class="alert alert-danger">
             <strong>Important!</strong> The Sphinx Node is currently unreachable!<br>
             Realms cannot be updated and are likely offline.
         </div>
     @endif
+
+    <div class="row">
+        <div class="col-md-8">
+            <h1>Hello, {{ Auth::user()->username }}!</h1>
+        </div>
+        <div class="col-md-4">
+            <table class="statistics-table table table-hover">
+                <tr>
+                    <td class="stat-name">Node Online?</td>
+                    <td class="stat-value">{!! $stats['nodeOnline'] ? '<span style="color:green;">Online</span>' : '<span style="color:red;">Offline</span>' !!}</td>
+                </tr>
+                <tr>
+                    <td class="stat-name">Realm Count</td>
+                    <td class="stat-value">{{ $stats['realmCount'] }}</td>
+                </tr>
+                <tr>
+                    <td class="stat-name">Servers Running</td>
+                    <td class="stat-value">{{ $stats['serversRunning'] }}</td>
+                </tr>
+                <tr>
+                    <td class="stat-name">Online Players</td>
+                    <td class="stat-value">{{ $stats['onlinePlayers'] }}</td>
+                </tr>
+            </table>
+        </div>
+    </div>
 @endsection
